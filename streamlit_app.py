e1a04fcf480e1018f2c1b63a8faa4c55dbe3b965
sinhakanchan <kanchan.sinha07@gmail.com>
2023-04-20 23:00:54 -0400

    Update streamlit_app.py
    new line

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
